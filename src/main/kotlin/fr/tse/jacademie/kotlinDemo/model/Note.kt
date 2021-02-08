a786e700e8c41404f7150e8b0a8871b809263848
Guillaume EHRET <guillaume@dev-mind.fr>
2021-02-08 14:18:58 +0100

    Different suggestions

--- src/main/kotlin/fr/tse/jacademie/kotlinDemo/model/Note.kt
+++ src/main/kotlin/fr/tse/jacademie/kotlinDemo/model/Note.kt
@@ -3,5 +3,4 @@ package fr.tse.jacademie.kotlinDemo.model
 import javax.persistence.Embeddable
 
 @Embeddable
-class Note(var option: String, var note: Double) {
-}
+class Note(var option: String, var note: Double)
